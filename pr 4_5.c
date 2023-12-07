06dcf1d6d1378739dc5ba7b3840d067828054746
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-07 17:09:40 +0530

    Update pr 4_5.c

--- pr 4_5.c
+++ pr 4_5.c
@@ -1,3 +1,13 @@
+/* 
+Q.5 Write a Program to print the below pattern using nested for loop.
+1                          1
+1 2                    2 1
+1 2 3              3 2 1
+1 2 3 4       4 3 2 1
+1 2 3 4 5 5 4 3 2 1
+*/
+
+
 #include <stdio.h>
 
  main() {
